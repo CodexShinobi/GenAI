141feae672cac54d808f2a46cdf80c687ce48ec4
Navneet <navneetprakash.op12@gmail.com>
2026-09-08 22:33:53 +0530

    Add JWT blacklist validation

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require ('mongoose')
+
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"user already exist"],
+        required:true,
+    },
+
+    email:{
+        type:String,
+        
+        unique:[true,"Account exist with current email add"],
+    required:true,
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+})
+
+
+const userModel= mongoose.model("users",userSchema)
+
+module.exports=userModel
